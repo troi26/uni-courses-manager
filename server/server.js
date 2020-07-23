fb275ba427a66c409c8d0d122aae45b200fb8ecf
Trayan D Troev <trayan97@gmail.com>
2020-07-23 13:45:48 +0300

    Cors enabled and body-parses added.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+var cors = require('cors')
+const bodyParser = require('body-parser');
 const usersRouter = require('./routes/users-router').router;
 const coursesRouter = require('./routes/courses-router');
 // const chatRouter = require('./routes/chat-router');
@@ -14,13 +16,38 @@ dotenv.config();
 const app = express();
 const port = 9010;
 
-app.use(express.json({limit: '50mb'}));
-
+// app.use(express.json({limit: '50mb'}));
 
 app.use(function (err, req, res, next) {
     console.error(err.stack)
     sendErrorResponse(req, res, 500, `Server error: ${err.message}`, err);
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.header('Access-Control-Allow-Origin', '*');
+  // Request methods you wish to allow
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type,__setXHR_');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.header('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
+
+// app.use(cors());
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   console.log(req.body);
+//   next(req);
+// });
 
 app
     .use('/api/courses', coursesRouter)
