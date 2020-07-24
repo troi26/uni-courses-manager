2338f713a693779b06808425cb7d72ec2737a0ba
Trayan D Troev <trayan97@gmail.com>
2020-07-24 11:40:46 +0300

    Implemented auth jwt checking middleware.

--- server/middlewares/auth.middleware.js
+++ server/middlewares/auth.middleware.js
@@ -2,6 +2,7 @@ const jwt = require('jsonwebtoken');
 
 function authenticateToken(req, res, next) {
     // Gather the jwt access token from the request header
+    console.log(req.headers.authorization);
     const authHeader = req.headers.authorization;
     const token = authHeader && authHeader.split(' ')[1];
     if (token == null) return res.sendStatus(401); // if there isn't any token
