9e56c5517efce8a789a6a3f04f076789da4820ef
Trayan D Troev <trayan97@gmail.com>
2020-07-24 14:17:34 +0300

    (WEB_APP): Implemented course modification API call.

--- web-app/src/api/courses.api.js
+++ web-app/src/api/courses.api.js
@@ -1,29 +1,6 @@
 import { domain } from "./server.connection";
 import axios from 'axios'; 
-import { buildTokenAuthHeader } from "./utils";
-
-// export const enrollIntoCourse = async (stuentId, courseId, token) => {
-//     const endpoint = `${domain}/enroll/${courseId}/${stuentId}`;
-//     try {
-//         fetch(endpoint, {
-//             method: "PATCH",
-//         }).then(res => {
-//             if (res.ok) {
-//                 return res.json();
-//             } else {
-//                 throw res.json();
-//             }
-//         }).then(data => {
-//             return data;
-//         }).catch(err => {
-//             throw err;
-//         });
-    
-//         // return modifiedCourse;
-//     } catch (err) {
-//         console.log(err);
-//     }
-// }
+import { buildTokenAuthHeader, buildPOSTWithJWTConfig } from "./utils";
 
 export const loadCourseById = async (courseId, token) => {
     try {
@@ -59,6 +36,16 @@ export const transferCourse = async (courseId, userFrom, userTo, token) => {
     try {
         const endpoint = `${domain}/api/courses/transfer/${courseId}/${userFrom}/${userTo}/`;
         const response = await axios.patch(endpoint, buildTokenAuthHeader(token));
+        return response.data;
+    } catch (err) {
+        throw err;
+    }
+}
+
+export async function modifyCourse (course, userId, token) {
+    try {
+        const endpoint = `${domain}/api/courses/${userId}/${course.id}`;
+        const response = await axios.put(endpoint, course, buildPOSTWithJWTConfig(token));
         return response.data;
     } catch (err) {
         throw err;
