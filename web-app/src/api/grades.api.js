2f387663f1947dca8bcb58cd2f11ea289068d1d7
Trayan D Troev <trayan97@gmail.com>
2020-07-24 12:50:07 +0300

    (WEB-APP): Implemented api calls for grade`s creation/modification and deletion.

--- web-app/src/api/grades.api.js
+++ web-app/src/api/grades.api.js
@@ -1,16 +1,56 @@
 import { domain } from "./server.connection";
 import axios from 'axios'; 
-import { buildTokenAuthHeader } from "./utils";
+import { buildTokenAuthHeader, buildPOSTWithJWTConfig } from "./utils";
+import { createPostRequest } from "./users.api";
+
+const buildEndpoint = (apiroute) => {
+    return `${domain}${apiroute}`;
+}
 
 export const loadGradesByCourse = async (courseId, token) => {
-    const endpoint = `${domain}/api/grades/bycourse/${courseId}`;
+    const endpoint = buildEndpoint(`/api/grades/bycourse/${courseId}`);
     try {
         const response = await axios(endpoint, buildTokenAuthHeader(token));
-        if (response.ok) {
-
-        }
-        return response.data;
+        // if (response.ok) {
+            return response.data;
+        // }
     } catch (err) {
         throw err;
     }
 };
+
+export const modifyGrade = async (data, token) => {
+    const endpoint = buildEndpoint(`/api/grades/${data.userId}/${data.id}`);
+    try {
+        const response = await axios.put(endpoint, data, buildPOSTWithJWTConfig(token));
+        // if (response.ok) {
+            return response.data;
+        // }
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const addGrade = async (data, token) => {
+    const endpoint = buildEndpoint(`/api/grades`);
+    try {
+        const response = await axios.post(endpoint, data, buildPOSTWithJWTConfig(token));
+        // if (response.ok) {
+            return response.data;
+        // }
+    } catch (err) {
+        throw err;
+    }
+}
+
+export const removeGrade = async (gradeId, token) => {
+    const endpoint = buildEndpoint(`/api/grades/${gradeId}`);
+    try {
+        const response = await axios.delete(endpoint, buildPOSTWithJWTConfig(token));
+        // if (response.ok) {
+            return response.data;
+        // }
+    } catch (err) {
+        throw err;
+    }
+}
--- web-app/src/api/utils.js
+++ web-app/src/api/utils.js
@@ -13,3 +13,12 @@ export const buildTokenAuthHeaderJSON = () => {
         }
     };
 }
+
+export const buildPOSTWithJWTConfig = (data, token) => {
+    return {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+        }
+    };
+}
